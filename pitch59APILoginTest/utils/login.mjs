811059e10a0f3a72d9d8c3b432b045c795a2bc76
Dev <samson.dev007@gmail.com>
2022-02-01 12:05:23 -0700

    I made the changes needed ro run the files

--- pitch59APILoginTest/utils/login.mjs
+++ pitch59APILoginTest/utils/login.mjs
@@ -1,28 +1,16 @@
-/*import assert from 'assert';
-import login from '../test/login.test.js';*/
-import fetch from 'node-fetch';
+import fetch from 'note-fetch';
 
-const login = async ({emailId, password}) => {
+const login = async ({userName, password}) => {
 
-    const options = {
+    const option = {
         method: 'POST',
-        body: JSON.stringify({emailId, password}),
-        header: {'Content-Type':'application/json'}
-    }
-
-    const apiResponse = await fetch('https://api.p59.dev',options);
-    const loginResponse = await apiResponse.text();
+        body: JSON.stringify({userName, password}),
+        headers: {'Contest-Type':'application/json'}
+    },
+    const apiResponse = await fetch('http://pitch59-url/api/account/login', option);
+    const apiResponse = await apiResponse.text();
 
     return loginResponse;
-
 }
 
 export default login;
-
-/*it("Login API should return a valid login",async ()=>{
-
-    const loginToken= await login({emailId:"myfriend@gmail.com",password:"YoDaddy"});
-
-    /*assert.equal(logintoken.length,36);/*this is wrong
-    assert.equal(errorWasCaught,false);
-})*/
